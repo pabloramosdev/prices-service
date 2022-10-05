7df0b47adc99fbda2441a06b6c2de07f6120533c
pablo <pabloramosdev@gmail.com>
2022-10-05 16:55:43 +0200

    test: unit and integration tests

---
+++ src/test/java/com/shop/prices/infrastructure/repository/h2/H2PriceRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.shop.prices.infrastructure.repository.h2;
+
+import com.shop.prices.domain.Price;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static com.shop.prices.utils.PriceUtils.*;
+import static com.shop.prices.utils.PriceUtils.assertEqualsPriceEntity;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@ExtendWith(SpringExtension.class)
+@ComponentScan(basePackageClasses = H2PriceRepository.class)
+class H2PriceRepositoryTest {
+
+    @Autowired
+    private H2PriceRepository h2PriceRepository;
+
+    @Autowired
+    private SpringDataH2PriceRepository springDataH2PriceRepository;
+
+    @BeforeEach
+    public void setUp() {
+        springDataH2PriceRepository.save(PRICE_ENTITY);
+    }
+
+    @Test
+    void findPriceBetweenDates() {
+        List<Price> prices = h2PriceRepository.findPriceBetweenDates(ZARA, PRODUCT_ID, LocalDateTime.now());
+        assertNotNull(prices);
+        assertEquals(1, prices.size());
+        assertEqualsPrice(PRICE, prices.get(0));
+    }
+}
